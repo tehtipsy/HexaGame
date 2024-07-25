ac536e0da06f53d904c752c2184051dac2496f40
tehtipsy <asafshmueli@gmail.com>
2024-07-25 21:40:06 +0300

    place tiles and pick up card

--- TileMapLayout.gd
+++ TileMapLayout.gd
@@ -1,10 +1,8 @@
 extends TileMap
 
 
-# Called when the node enters the scene tree for the first time.
-
 @export var map_width: int = 15
-@export var map_height: int = 7
+@export var map_height: int = 6
 @export var tile_types: int = 3  # Adjust this to match the number of different tile types you have
 # @export var alt_types: int = 3
 
@@ -34,7 +32,7 @@ func _process(_delta):
 
 func _handle_arrow(instruction, arrow_layer, arrow_id, tile_position):
 	var current_arrow_atlas_coords = get_cell_atlas_coords(arrow_layer, tile_position)
-	print("arrow atlas coords: ", current_arrow_atlas_coords)
+	#print("arrow atlas coords: ", current_arrow_atlas_coords)
 	if current_arrow_atlas_coords.x >= 0:
 		var new_in_arrow_atlas_coords = Vector2i(current_arrow_atlas_coords.x + 1, current_arrow_atlas_coords.y)
 		if new_in_arrow_atlas_coords.x > 5:
@@ -46,28 +44,36 @@ func _handle_arrow(instruction, arrow_layer, arrow_id, tile_position):
 		var arrow_atlas_coords = Vector2i(0, 0)
 		set_cell(arrow_layer, tile_position, arrow_id, arrow_atlas_coords)
 
-func _handle_click(instruction, arrow_layer, arrow_id, tile_position):
+func _place_card(tile_position):
+	var tile_atlas_coords = Vector2i(0,0) # get from card holder
+	set_cell(tile_color_layer, tile_position, tile_color_id, tile_atlas_coords)
+
+
+func _handle_click(tile_position):
+# func _handle_click(instruction, arrow_layer, arrow_id, tile_position):
 	# Get the tile data
 	var tile_data = get_cell_tile_data(tile_color_layer, tile_position)
 	if tile_data:
 		var current_source_id = get_cell_source_id(tile_color_layer, tile_position)
 		var current_atlas_coords = get_cell_atlas_coords(tile_color_layer, tile_position)
-		print("Tile source_id: ", current_source_id)
-		print("Tile atlas coords: ", current_atlas_coords)
-		if current_atlas_coords.x == 2 and current_source_id == 0: # white tile
-			_handle_arrow(instruction, arrow_layer, arrow_id, tile_position)
+		#print("Tile source_id: ", current_source_id)
+		#print("Tile atlas coords: ", current_atlas_coords)
+		# if current_atlas_coords.x == 2 and current_source_id == 0: # white tile
+		# 	_handle_arrow(instruction, arrow_layer, arrow_id, tile_position)
+		_place_card(tile_position)
 
-#func _input(event):
-	#if event is InputEventMouseButton:
-		#var _global_position = get_global_mouse_position()
-		#var tile_position = local_to_map(to_local(_global_position))
-		#print("Clicked tile: ", tile_position)
+func _input(event):
+	if event is InputEventMouseButton:
+		var _global_position = get_global_mouse_position()
+		var tile_position = local_to_map(to_local(_global_position))
 		#print("Tile X: ", tile_position.x)
-		#if !Input.is_key_pressed(KEY_SHIFT):
-			#if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-				#_handle_click('add', tile_in_arrow_layer, tile_in_arrow_id, tile_position)
-			#if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
-				#_handle_click('remove', tile_in_arrow_layer, tile_in_arrow_id, tile_position)
+		if !Input.is_key_pressed(KEY_SHIFT):
+			if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+				# print("Clicked tile: ", tile_position)
+				# _handle_click('add', tile_in_arrow_layer, tile_in_arrow_id, tile_position)
+				_handle_click(tile_position)
+			# if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
+			# 	_handle_click('remove', tile_in_arrow_layer, tile_in_arrow_id, tile_position)
 		#else:
 			#print('SHIFTING')
 			#if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
--- card_container.gd
+++ card_container.gd
@@ -18,26 +18,35 @@ func _on_mouse_exited():
 	is_higlighted = false
 
 
-func _on_gui_input(event):
+func _input(event):
 	if (event is InputEventMouseButton) and (event.button_index == 1):
 		if event.button_mask == 1:
-			if is_higlighted:
-				var _card = card.instanciate()
-				get_tree().get_root().get_node("CardHolder").add_child(_card)
-				# hide card sprite in container
-				self.get_child(0).hide()
+			#print("in gui")
+			#print(is_higlighted)
+			#if is_higlighted:
+				#var _card = card.instanciate()
+				#get_tree().get_root().get_node("Game/CardHolder").add_child(_card)
+			get_tree().get_root().get_node("Game/CardHolder").get_child(0).show()
+			# hide card sprite in container
+			self.get_child(0).hide()
 		elif event.button_mask == 0:
 			# check for valid placement
-			var tile_map = get_tree().get_root().get_node("TileMap")
-			var max_x = tile_map.map_width
-			var max_y = tile_map.map_hight
-
+			var tile_map = get_tree().get_root().get_node("Game/TileMap")
+			#print(tile_map)
+			var max_x = tile_map.get("map_width")
+			var max_y = tile_map.get("map_height")
+			#print(max_x)
 			var _global_position = get_global_mouse_position()
 			var tile_position = tile_map.local_to_map(tile_map.to_local(_global_position))
-
+			print(tile_position)
+			print(typeof(max_x))
+			print(typeof(max_y))
+			print(max_y)
+			print(typeof(tile_position.x))
 			var is_placement_valid: bool
-			if (tile_position.x <= max_x) and (tile_position.y <= max_y):
+			if (0 <= tile_position.x and tile_position.x <= max_x) and (0 <= tile_position.y and tile_position.y <= max_y):
 				is_placement_valid = true
+
 
 			if !is_placement_valid:
 			# return card to card container
